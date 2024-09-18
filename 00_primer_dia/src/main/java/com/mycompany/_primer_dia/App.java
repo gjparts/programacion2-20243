e8d57ee02e60150af4a7c76d36d20c52f19c7f12
GJParts <39961515+gjparts@users.noreply.github.com>
2024-09-18 11:58:09 -0600

    commit 1809202431476

---
+++ 00_primer_dia/src/main/java/com/mycompany/_primer_dia/App.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._primer_dia;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hola UNAH");
+        System.out.print("Texto random");
+        System.out.println("Abc");
+        System.out.println("Xyz");
+        System.out.println("123");
+        //puede usar caracteres de escape
+        //\n es para nueva linea
+        System.out.println("Gerardo\nPortillo\nSistemas\nUNAH");
+        //tabulacion:
+        System.out.println("Nombre\tEdad\tProfesion");
+        System.out.println("Josue\t42\tProgramador");
+        System.out.println("Irene\t40\tChef");
+        //poder imprimir el slash inverso
+        System.out.println("Esto es un slash inverso: \\");
+        //imprimir solo un salto de linea
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println("Adios");
+        
+    }
+}
