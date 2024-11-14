55b364fa5e6c52ab76078ef41cc66338dedbdaa9
gjparts <gjparts@gmail.com>
2024-11-14 10:54:38 -0600

    commit 1411202412251

---
+++ 41_gui01/src/main/java/com/mycompany/_gui01/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._gui01;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class App {
+
+    public static void main(String[] args) {
+        //instanciar un nuevo objeto de clase Principal
+        Principal x = new Principal();
+        //dibujar en pantalla el objeto x
+        x.setVisible(true);
+    }
+}
