a245caa3f22236023a34eb499877f55c1c6abdf5
GJParts <39961515+gjparts@users.noreply.github.com>
2024-11-18 11:48:05 -0600

    commit 181120244444

---
+++ 42_gui02/src/main/java/com/mycompany/_gui02/App.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._gui02;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class App {
+
+    public static void main(String[] args) {
+        //crear una instancia de Ventana1 (objeto)
+        Ventana1 x = new Ventana1();
+        //mandar a dibujar en pantalla a x
+        x.setVisible(true);
+    }
+}
