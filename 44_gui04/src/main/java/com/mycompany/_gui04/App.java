6ae969166194d3593c69ce452aea5edecdacccaa
gjparts <gjparts@gmail.com>
2024-11-26 10:54:53 -0600

    commit 2611202422948

---
+++ 44_gui04/src/main/java/com/mycompany/_gui04/App.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._gui04;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Cuadratica qwe = new Cuadratica();
+        qwe.setVisible(true);
+    }
+}
