05fe44f71fcd332dc653af0e558807a255aa5f6e
gjparts <gjparts@gmail.com>
2024-12-03 11:00:02 -0600

    commit 03122024141

---
+++ 47_gui07/src/main/java/com/mycompany/_gui07/App.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._gui07;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Formulario f = new Formulario();
+        f.setVisible(true);
+    }
+}
