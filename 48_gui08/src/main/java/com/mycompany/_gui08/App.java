feb3e745bbee4618997601be5737a27b22a6816f
gjparts <gjparts@gmail.com>
2024-12-04 11:11:16 -0600

    commit 0412202422338

---
+++ 48_gui08/src/main/java/com/mycompany/_gui08/App.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._gui08;
+
+/**
+ *
+ * @author Gerardo
+ */
+public class App {
+
+    public static void main(String[] args) {
+        Ventana1 abc = new Ventana1();
+        abc.setVisible(true);
+    }
+}
